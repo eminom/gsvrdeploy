5df5ecb956b06bcd325e4f2e2a6cb7e171e94703
baihai <57270348@qq.com>
2017-03-06 15:06:17 +0800

    deploy done

--- src/deploy/maestro.go
+++ src/deploy/maestro.go
@@ -1,142 +1,221 @@
-
 package main
 
 import (
+	"crypto/md5"
 	"fmt"
-	"os/signal"
+	"github.com/OneOfOne/xxhash"
+	"io"
 	"os"
+	"os/exec"
+	"os/signal"
+	"path/filepath"
+	"strings"
 	"thread"
-    "path/filepath"
-    "crypto/md5"
-    "io"
-    "strings"
-    "github.com/OneOfOne/xxhash"
+	"time"
 )
 
-
-func CalcFileMd5(path string)string{
-    fin, err := os.Open(path)
-    if nil != err {
-        panic(err)
-    }
-    defer fin.Close()
-    h := md5.New()
-    _, e2 := io.Copy(h, fin);
-    if nil != e2 {
-        panic(err)
-    }
-    // 总是32位宽的16进制的数字;
-    return fmt.Sprintf("%x", h.Sum(nil))
+type calcPackIn struct {
+	abs_path string
+	relpath  string
+	fi       os.FileInfo // interface
 }
 
-func CalcFileXXHash(path string)string{
-    fin, err := os.Open(path)
-    if nil != err {
-        panic(err)
-    }
-    defer fin.Close()
-    h := xxhash.NewS32(10241024)   // New32(注意是10进制)
-    _, e2 := io.Copy(h, fin)
-    if nil != e2 {
-        panic(e2)
-    }
-    return fmt.Sprintf("%08x", h.Sum32())
+type calcPackOut struct {
+	hashName string
+	size     int
+	xxhash   string
+	relpath  string
+	abspath  string
+}
+
+func (self *calcPackOut) toStr() string {
+	return fmt.Sprintf("%v\t%v\t%v\t%v", self.hashName, self.size, self.xxhash, self.relpath)
+}
+
+func CalcFileMd5(path string) string {
+	fin, err := os.Open(path)
+	if nil != err {
+		panic(err)
+	}
+	defer fin.Close()
+	h := md5.New()
+	_, e2 := io.Copy(h, fin)
+	if nil != e2 {
+		panic(err)
+	}
+	// MD5:总是32位宽的16进制的数字;
+	return fmt.Sprintf("%x", h.Sum(nil))
+}
+
+func CalcFileXXHash(path string) string {
+	fin, err := os.Open(path)
+	if nil != err {
+		panic(err)
+	}
+	defer fin.Close()
+	h := xxhash.NewS32(10241024) // New32(注意是10进制)
+	_, e2 := io.Copy(h, fin)
+	if nil != e2 {
+		panic(e2)
+	}
+	return fmt.Sprintf("%08x", h.Sum32())
 }
 
 //定制标准ENTRY
-func CalcFileEnt(abs_path string, relpath string, fi os.FileInfo)string{
-    md5 := CalcFileMd5(abs_path)
-    xxhash := CalcFileXXHash(abs_path)
-    relpath = strings.Replace(relpath, "\\", "/", -1)
-    return fmt.Sprintf("%s%s\t%d\t%s\t%s", md5, filepath.Ext(fi.Name()), fi.Size(), xxhash, relpath)
-    //return fmt.Sprintf("%s", xxhash)
+func calcFileEnt(abs_path string, relpath string, fi os.FileInfo) interface{} {
+	md5 := CalcFileMd5(abs_path)
+	xxhash := CalcFileXXHash(abs_path)
+	relpath = strings.Replace(relpath, "\\", "/", -1)
+	//return fmt.Sprintf("%s%s\t%d\t%s\t%s", md5, filepath.Ext(fi.Name()), fi.Size(), xxhash, relpath)
+	outName := fmt.Sprintf("%s%s", md5, filepath.Ext(fi.Name()))
+	return &calcPackOut{outName, int(fi.Size()), xxhash, relpath, abs_path}
 }
 
-type CalcPack struct {
+func hacerLaCama(p interface{}) interface{} {
+	fpath := p.(*calcPackIn)
+	return calcFileEnt(fpath.abs_path, fpath.relpath, fpath.fi)
+}
+
+var meTargetPath string
+
+func formatTargetPath(n string) string {
+	return fmt.Sprintf("%v\\%v", meTargetPath, n)
+}
+
+type copyCmd struct {
+	newName  string
 	abs_path string
-	relpath string
-	fi os.FileInfo  // interface
 }
 
-func (self *CalcPack)Print(){
-	fmt.Printf("<%s>\n", self.fi.Name())
+func (self *copyCmd) do() {
+	err := exec.Command("cmd",
+		"/C",
+		"copy",
+		self.abs_path,
+		formatTargetPath(self.newName),
+		"/Y",
+	).Run()
+	if nil != err {
+		fmt.Println("Error for copy")
+		fmt.Printf("%v\n%v\n", self.abs_path, self.newName)
+		panic(err)
+	}
 }
 
-func hacerLaCama(p interface{})interface{}{
-	fpath := p.(*CalcPack)
-	res := CalcFileEnt(fpath.abs_path, fpath.relpath, fpath.fi)
-	return &res
+func doOneCopy(p interface{}) interface{} {
+	ccmd := p.(*copyCmd)
+	//fmt.Printf("Do-copy <%v> to <%v>\n", ccmd.abs_path, ccmd.newName)
+	ccmd.do()
+	return 1
 }
 
-func getAllFiles(root string)[]*CalcPack{
-	files := make([]*CalcPack, 0)
-    file_count, dir_count := 0, 0
-    err := filepath.Walk(root, func(patho string, fi os.FileInfo, inpErr error)(err error){
-    	if nil != inpErr {
-    		panic(inpErr)
-    	}
-        path := strings.TrimPrefix(patho, root)
-        //~ both
-        path = strings.TrimPrefix(path, "/")
-        path = strings.TrimPrefix(path, "\\")
-        if fi.IsDir() {
-            dir_count += 1
-        } else {
-            file_count += 1
-            abs, _ := filepath.Abs(filepath.Join(root, path))
-            files = append(files, &CalcPack{abs, path, fi})
-        }
-        return nil
-    })
-    if nil != err {
-        panic(err)
-    }
-    return files
+/////////////////////////////////////////////////
+//~ The list for current deploying version
+func getAllFiles(root string) []*calcPackIn {
+	files := make([]*calcPackIn, 0)
+	file_count, dir_count := 0, 0
+	err := filepath.Walk(root, func(patho string, fi os.FileInfo, inpErr error) (err error) {
+		if nil != inpErr {
+			panic(inpErr)
+		}
+		path := strings.TrimPrefix(patho, root)
+		//~ both
+		path = strings.TrimPrefix(path, "/")
+		path = strings.TrimPrefix(path, "\\")
+		if fi.IsDir() {
+			dir_count += 1
+		} else {
+			file_count += 1
+			abs, _ := filepath.Abs(filepath.Join(root, path))
+			files = append(files, &calcPackIn{abs, path, fi})
+		}
+		return nil
+	})
+	if nil != err {
+		panic(err)
+	}
+	return files
+}
+
+func doWriteToVersionFile(arr []string) {
+	name := getTargetVersionFile()
+	f, err := os.OpenFile(name, os.O_CREATE|os.O_WRONLY, 0644)
+	defer f.Close()
+	if err != nil {
+		panic(err)
+	}
+	for _, line := range arr {
+		fmt.Fprintf(f, "%v\n", line)
+	}
 }
 
 ////////////////////////////////////////////////////////////////////////////
 func main() {
-    if len(os.Args) < 2 {
-        fmt.Println("Need more input parameter <folder-name>")
-        os.Exit(1)
-    }
-    target_dir := os.Args[1]
-    di, dErr := os.Stat(target_dir)
-    if nil != dErr {
-        panic(dErr)
-    }
-    if ! di.IsDir() {
-        fmt.Printf("`%s' is not a valid directory.\n", target_dir)
-        os.Exit(2)
-    }
+	start := time.Now()
+	if len(os.Args) < 2 {
+		fmt.Println("Need more input parameter <folder-name>")
+		os.Exit(1)
+	}
+	target_dir := os.Args[1]
+	di, dErr := os.Stat(target_dir)
+	if nil != dErr {
+		panic(dErr)
+	}
+	if !di.IsDir() {
+		fmt.Printf("`%s' is not a valid directory.\n", target_dir)
+		os.Exit(2)
+	}
 	allFiles := getAllFiles(target_dir)
 	expected := len(allFiles)
-	resList := make(chan interface{}, expected)
+
+	meTargetPath = getTargetPath()
+	//~  THE OUPUT
+	outs := make(chan interface{}, 12)
+
+	//~ register methods for route's processing
+	threads.DoInit()
+	threads.RegisterWorkRoutine(&calcPackIn{}, hacerLaCama)
+	threads.RegisterWorkRoutine(&copyCmd{}, doOneCopy)
+
+	inputChan := make(chan interface{}, expected*2)
 	workpool := threads.MakeWorkPool()
-	wakeUno := workpool.Start(threads.DefaultWorkCount, expected, hacerLaCama, resList)
+	workpool.StartPool(threads.DefaultWorkCount, inputChan, outs)
+
 	fullOuts := make([]string, 0)
 	c := make(chan os.Signal, 1)
 	signal.Notify(c, os.Interrupt, os.Kill)
 
-    //~ 全部放进去再说
-	for _, f := range allFiles{
-		wakeUno <- f
+	//~ all in
+	for _, f := range allFiles {
+		inputChan <- f
 	}
-	left := expected
-	Main100: for ;left>0;{
+	left := expected * 2
+
+Main100:
+	for left > 0 {
 		select {
-		case resOut := <- resList:
-			left--
-			fullOuts = append(fullOuts, *resOut.(*string))
-		case <- c:
+		case resOut := <-outs:
+			var dc int = 1
+			switch resOut.(type) {
+			case int:
+				break
+			case *calcPackOut:
+				po := resOut.(*calcPackOut)
+				fullOuts = append(fullOuts, po.toStr())
+				inputChan <- &copyCmd{po.hashName, po.abspath}
+				break
+			default:
+				panic("error")
+				dc = 0
+			}
+			left -= dc
+		case <-c:
 			break Main100
 		default: //~ this is so necessary.(or blocked above)
 		}
 	}
 	workpool.Shutdown()
-    //workpool.PrintInfo()
-	for _, v := range fullOuts{
-        fmt.Println(v)
-    }
-
+	//workpool.PrintInfo()
+	doWriteToVersionFile(fullOuts)
+	fmt.Println(time.Since(start))
 }
--- src/thread/threads.go
+++ src/thread/threads.go
@@ -1,91 +1,109 @@
-
 package threads
 
 import (
-    "sync"
-    _ "time"
-    "fmt"
+	"fmt"
+	"reflect"
+	"sync"
+	_ "time"
 )
 
+type WorkRoutine func(interface{}) interface{}
+
+type Processor struct {
+	wr WorkRoutine
+}
+
+var processors map[interface{}]*Processor
+
+func DoInit() {
+	processors = make(map[interface{}]*Processor)
+}
+
+func RegisterWorkRoutine(m interface{}, wr WorkRoutine) {
+	typeEste := reflect.TypeOf(m)
+	processors[typeEste] = &Processor{wr}
+}
+
+func getProcessor(m interface{}) *Processor {
+	return processors[reflect.TypeOf(m)]
+}
+
 type workUnit struct {
-    Intv int
-    RefCounter int
-    Name string
-    IsClosing *chan bool
-    WorkFunc interface{}
-    wg sync.WaitGroup
+	Intv       int
+	RefCounter int
+	Name       string
+	IsClosing  *chan bool
+	wg         sync.WaitGroup
 }
 
-func makeWorkUnit(n int) *workUnit{
-    rv := &workUnit{Intv:n, Name:fmt.Sprintf("<%v>", n)}
-    isClosin := make(chan bool)
-    rv.IsClosing = &isClosin
-    return rv
+func makeWorkUnit(n int) *workUnit {
+	rv := &workUnit{Intv: n, Name: fmt.Sprintf("<%v>", n)}
+	isClosin := make(chan bool)
+	rv.IsClosing = &isClosin
+	return rv
 }
 
-func (self *workUnit)Start(do func(interface{})interface{}, in chan interface{}, out chan interface{}) {
-    self.WorkFunc = do
-    self.wg.Add(1)
-    go self._workRoutine(in, out)
+func (self *workUnit) StartWork(in chan interface{}, out chan interface{}) {
+	self.wg.Add(1)
+	go self._workRoutine(in, out)
 }
 
-func (self *workUnit)_workRoutine(in chan interface{}, out chan interface{}) {
-    //intv := self.Intv
-    refInt := &self.RefCounter
-    worker := self.WorkFunc
-    Main100:for {
-        //time.Sleep(time.Duration(intv * 666) * time.Millisecond)
-        select {
-        case verUno := <- in:
-            resI := worker.(func(interface{})interface{})(verUno)
-            out <- resI
-            *refInt++
-        case <- *self.IsClosing:
-            break Main100
-        }
-    }
-    self.wg.Done()
+func (self *workUnit) _workRoutine(in chan interface{}, out chan interface{}) {
+	refInt := &self.RefCounter
+Main100:
+	for {
+		select {
+		case uno := <-in:
+			res := callToFunc(uno)
+			out <- res
+			*refInt++
+		case <-*self.IsClosing:
+			break Main100
+		}
+	}
+	self.wg.Done()
 }
 
-type WorkUnitPool struct{
-    objs []*workUnit
+func callToFunc(m interface{}) interface{} {
+	proc := getProcessor(m)
+	return proc.wr(m)
+}
+
+type WorkUnitPool struct {
+	objs []*workUnit
 }
 
 func MakeWorkPool() *WorkUnitPool {
-    rv := &WorkUnitPool{}
-    return rv
+	rv := &WorkUnitPool{}
+	return rv
 }
 
-func (self *WorkUnitPool)Start(size int, inSize int, do func(interface{})interface{}, out chan interface{}) chan interface{} {
-    waker := make(chan interface{}, inSize)
-    self.objs = make([]*workUnit, size)
-    wu := self.objs
-    for i:=0;i<len(wu);i++{
-        wu[i] = makeWorkUnit(i)
-    }
-    for _, obj := range wu {
-        obj.Start(do, waker, out)
-    }
-    return waker
+func (self *WorkUnitPool) StartPool(size int, in chan interface{}, out chan interface{}) {
+	self.objs = make([]*workUnit, size)
+	wu := self.objs
+	for i := 0; i < len(wu); i++ {
+		wu[i] = makeWorkUnit(i)
+	}
+	for _, obj := range wu {
+		obj.StartWork(in, out)
+	}
 }
 
-func (self *WorkUnitPool)Shutdown(){
-    for _, obj := range self.objs {
-        *obj.IsClosing <- true
-        //fmt.Printf("Waiting for <%v>\n", obj.Name)
-        obj.wg.Wait()
-    }
-    //? fmt.Println("Pool closed")
+func (self *WorkUnitPool) Shutdown() {
+	for _, obj := range self.objs {
+		*obj.IsClosing <- true
+		//fmt.Printf("Waiting for <%v>\n", obj.Name)
+		obj.wg.Wait()
+	}
+	//? fmt.Println("Pool closed")
 }
 
-
-func (self *WorkUnitPool)PrintInfo() {
-    for _, obj := range self.objs {
-        fmt.Printf("<%v> is waken for %v time(s)\n", obj.Name, obj.RefCounter)
-    }
+func (self *WorkUnitPool) PrintInfo() {
+	for _, obj := range self.objs {
+		fmt.Printf("<%v> is waken for %v time(s)\n", obj.Name, obj.RefCounter)
+	}
 }
-
 
 const (
-    DefaultWorkCount = 4
+	DefaultWorkCount = 256
 )
